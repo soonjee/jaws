59710a8d4891cbc2b7d58c25c3347c1f3b49ed0e
soonji <youinxxs@naver.com>
2022-08-05 15:53:05 +0900

    20220805

--- js/jaws.js
+++ js/jaws.js
@@ -1,9 +1,9 @@
-$(function (){
-    $('.main_slides').slick ({
-        // autoplay: true,
-        arrows:false,
-        speed:2000,
-        dots:false,
-        fade:true,
+$(function () {
+    $('.sns_slides').slick({
+        autoplay: true,
+        arrows: false,
+        speed: 2000,
+        dots: false,
+        fade: true,
     })
 })
